08a72af8a16eecb8beb07e7848ba31ea05c008a0
DevPH-Santos <pedrohenriquerod.santos@gmail.com>
2026-05-18 01:00:27 -0300

    SPRINT 6-> Configurar conexão com banco de dados

---
+++ src/server.js
@@ -0,0 +1,22 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import app from "./app.js";
+import "./config/db.js";
+import pool from "./config/db.js";
+
+const PORT = process.env.PORT || 3000;
+
+try {
+  const connection = await pool.getConnection();
+
+  console.log("✅ Conectado ao MySQL!");
+
+  connection.release();
+} catch (error) {
+  console.error("❌ Erro ao conectar:", error.message);
+}
+
+app.listen(PORT, () => {
+  console.log(`🚀 Servidor rodando na porta ${PORT} -> localhost:${PORT}`);
+});
